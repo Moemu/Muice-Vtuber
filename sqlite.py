fb56deccb6938b18b7ff6c41708f92e0bf2ddee8
Moemu <WhitemuTeam@outlook.com>
2024-09-03 16:22:56 +0800

    Update: sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -4,45 +4,35 @@ class Database:
     def __init__(self) -> None:
         if not os.path.isfile('database.db'):
             self.__create_database()
+            self.connection = sqlite3.connect('database.db')
+            self.cursor = self.connection.cursor()
 
     def __create_database(self) -> None:
-        connection = sqlite3.connect('database.db')
-        cursor = connection.cursor()
-        cursor.execute('''CREATE TABLE CHAT(
+        self.cursor.execute('''CREATE TABLE CHAT(
             ID INT PRIMARY KEY NOT NULL,
             TIME TEXT NOT NULL,
             USERNAME TEXT NOT NULL,
             USERID INT NOT NULL,
             DANMU TEXT NOT NULL,
             RESPOND TEXT NOT NULL);''')
-        connection.commit()
-        connection.close()
+        self.connection.commit()
 
     def __get_lastest_id(self) -> int:
-        connection = sqlite3.connect('database.db')
-        cursor = connection.cursor()
-        data_cursor = cursor.execute('''SELECT * FROM CHAT ORDER BY ID DESC LIMIT 1''')
+        data_cursor = self.cursor.execute('''SELECT * FROM CHAT ORDER BY ID DESC LIMIT 1''')
         lastest_id = 0
         for data in data_cursor:
             lastest_id = data[0]
-        connection.close()
         return lastest_id
     
     def add_item(self, Username:str, Userid:int, Danmu:str, respond:str):
         current_time = time.strftime('%Y.%m.%d %H:%M:%S')
         available_id = self.__get_lastest_id() + 1
-        connection = sqlite3.connect('database.db')
-        cursor = connection.cursor()
-        cursor.execute(f'''INSERT INTO CHAT VALUES ({available_id}, '{current_time}', '{Username}', {Userid}, '{Danmu}', '{respond}');''')
-        connection.commit()
-        connection.close()
+        self.cursor.execute(f'''INSERT INTO CHAT VALUES ({available_id}, '{current_time}', '{Username}', {Userid}, '{Danmu}', '{respond}');''')
+        self.connection.commit()
 
     def get_history(self) -> list:
-        connection = sqlite3.connect('database.db')
-        cursor = connection.cursor()
-        data_cursor = cursor.execute('''SELECT * FROM CHAT''')
+        data_cursor = self.cursor.execute('''SELECT * FROM CHAT''')
         history = []
         for data in data_cursor:
             history.append([data[4],data[5]])
-        connection.close()
         return history
